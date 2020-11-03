d14d4fe518ddcf8d420d2cb0edd53d39368d4e85
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-11-03 17:47:02 +0000

    Created empty methods. Everything is running

--- user.js
+++ user.js
@@ -3,4 +3,26 @@ class User {
     this.name = name;
     this.date_of_birth = date_of_birth;
   }
+
+  age () {
+    return "age"
+  }
+
+  next_birthday () {
+    return "next birthday"
+  }
 }
+
+tests = [
+  new Date(1986, 0, 1),
+  new Date(1988, new Date().getMonth(), new Date().getDate()),
+  new Date(1990, 11, 30)
+]
+
+tests.forEach((date) => {
+  console.log(`${date} => ${new User("Test", date).age()}`)
+})
+
+tests.forEach((date) => {
+  console.log(`${date} => ${new User("Test", date).next_birthday()}`)
+})
