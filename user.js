a62797882b4b486e79de2c3b67968c90a2befab9
Benedetta Arinci <benedetta.arinci@gmail.com>
2020-11-03 18:06:20 +0000

    A bit of final refactoring

--- user.js
+++ user.js
@@ -2,31 +2,31 @@ class User {
   constructor(name, date_of_birth) {
     this.name = name;
     this.date_of_birth = date_of_birth;
+    this.userBMonth = this.date_of_birth.getMonth()
+    this.userBDay = this.date_of_birth.getDate()
+    this.userByear = this.date_of_birth.getFullYear()
   }
 
   age () {
-    let userByear = this.date_of_birth.getFullYear()
     let todayYear = new Date().getFullYear()
     if (this._isBirthdayPassed()) {
-      return todayYear - userByear
-    } else { return (todayYear - userByear - 1)}
+      return todayYear - this.userByear
+    } else { return (todayYear - this.userByear - 1)}
   }
 
   next_birthday () {
     let todayYear = new Date().getFullYear()
     if(this._isBirthdayPassed()) {
-      return new Date(todayYear+1, this.date_of_birth.getMonth(), this.date_of_birth.getDate())
+      return new Date(todayYear+1, this.userBMonth, this.userBDay)
     } else {
-      return new Date(todayYear, this.date_of_birth.getMonth(), this.date_of_birth.getDate())
+      return new Date(todayYear, this.userBMonth, this.userBDay)
     }
   }
 
   _isBirthdayPassed () {
     let todayMonth = new Date().getMonth()
     let todayDay = new Date().getDate()
-    let userBDay = this.date_of_birth.getDate()
-    let userBMonth = this.date_of_birth.getMonth()
-    return (todayMonth > userBMonth || (userBMonth === todayMonth && todayDay >= userBDay))
+    return (todayMonth > this.userBMonth || (this.userBMonth === todayMonth && todayDay >= this.userBDay))
   }
 }
 
